042b25157471aa65c1bf91bb9f5779fa64b85772
杨捷 <yangjie@jojoin.com>
2014-12-15 16:25:24 +0800

    Create tppl.js

---
+++ tppl.js
@@ -0,0 +1,31 @@
+
+function tppl(tpl, data){
+  var fn =  function (d) {
+    var i, k = ['$'], v = [[]];
+    for(i in d) {
+      k.push(i);
+      v.push(d[i]);
+    }
+    return (new Function(k, fn.$)).apply(d, v);
+  };
+
+  if(!fn.$){
+    fn.$ = '';
+    var tpls = tpl.replace(/[\r\t\n]/g, " ").replace(/\'/g,"\\'").split('[:')
+      , i = 0
+    while(i<tpls.length){
+      var p = tpls[i];
+      if(i){
+        var x = p.indexOf(':]');
+        fn.$ += p.substr(0, x);
+        p = p.substr(x+2)
+      }
+      fn.$ += "$+='"+p.replace(/\[\=\:(.*?)\:\]/g, "'+$1+'")+"';";
+      //log(p);
+      i++;
+    }
+    fn.$ += "return $";
+  }
+
+  return data ? fn(data) : fn;
+}
