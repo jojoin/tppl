06867ff1c6f2b33e2750b105f74578340ca6a158
杨捷 <yangjie@jojoin.com>
2014-12-23 13:46:16 +0800

    Update tppl.js

--- tppl.js
+++ tppl.js
@@ -13,36 +13,37 @@
  *
  */
 
-function tppl(tpl, data, fast){
-  var fn =  function (d, f) {
-    if(f||fast){
-        fn.$$ = fn.$$ || new Function(fn.$);
-        return fn.$$.apply(d);
-    }else{
-      var i, k = [], v = [];
-      for (i in d) {
-          k.push(i);
-          v.push(d[i]);
-      };
-      return (new Function(k, fn.$)).apply(d, v);
-    }
-  };
-  if(!fn.$){
-    fn.$ = 'var $="";';
-    var tpls = tpl.replace(/[\r\t\n]/g, " ").replace(/\'/g,"\\'").split('[:')
-      , i = 0
-    while(i<tpls.length){
-      var p = tpls[i];
-      if(i){
-        var x = p.indexOf(':]');
-        fn.$ += p.substr(0, x);
-        p = p.substr(x+2)
-      }
-      fn.$ += "$+='"+p.replace(/\[\=\:(.*?)\:\]/g, "'+$1+'")+"';";
-      i++;
-    }
-    fn.$ += "return $";
-  }
 
-  return data ? fn(data) : fn;
+function tppl(tpl, data, fast){
+    var fn =  function (d, f) {
+        if(f||fast){
+            fn.$$ = fn.$$ || new Function(fn.$);
+            return fn.$$.apply(d);
+        }else{
+            var i, k = [], v = [];
+            for (i in d) {
+                k.push(i);
+                v.push(d[i]);
+            };
+            return (new Function(k, fn.$)).apply(d, v);
+        }
+    };
+    if(!fn.$){
+        fn.$ = 'var $="";';
+        var tpls = tpl.replace(/[\r\t\n]/g, " ").split('[:')
+            , i = 0
+        while(i<tpls.length){
+            var p = tpls[i];
+            if(i){
+                var x = p.indexOf(':]');
+                fn.$ += p.substr(0, x);
+                p = p.substr(x+2)
+            }
+            fn.$ += "$+='"+p.replace(/\'/g,"\\'").replace(/\[\=\:(.*?)\:\]/g, "'+$1+'")+"';";
+            i++;
+        }
+        fn.$ += "return $";
+    }
+
+    return data ? fn(data) : fn;
 }
