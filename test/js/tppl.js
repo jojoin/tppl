0f39282c664ab0d2287cd25f67d19c6946a8052b
杨捷 <yangjie@jojoin.com>
2016-06-24 15:00:48 +0800

    fast

--- test/js/tppl.js
+++ test/js/tppl.js
@@ -1,28 +1,33 @@
-
 function tppl(tpl, data, fast){
-    var fn =  function(d) {
-        var i, k = [], v = [];
-        for (i in d) {
-            k.push(i);
-            v.push(d[i]);
-        };
-        return (new Function(k, fn.$)).apply(d, v);
-    };
-    if(!fn.$){
-        var tpls = tpl.split('[:');
-        fn.$ = "var $=''";
-        for(var t = 0;t < tpls.length;t++){
-            var p = tpls[t].split(':]');
-            if(t!=0){
-                fn.$ += '='==p[0].charAt(0)
-                  ? "+("+p[0].substr(1)+")"
-                  : ";"+p[0].replace(/\r\n/g, '')+"$=$"
-            }
-            // 支持 <pre> 和 [::] 包裹的 js 代码
-            fn.$ += "+'"+p[p.length-1].replace(/\'/g,"\\'").replace(/\r\n/g, '\\n').replace(/\n/g, '\\n').replace(/\r/g, '\\n')+"'";
-        }
-        fn.$ += ";return $;";
-        // log(fn.$);
+  var fn =  function (d) {
+    if(fast){
+        fn.$$ = fn.$$ || new Function(fn.$);
+        return fn.$$.apply(d);
+    }else{
+      var i, k = [], v = [];
+      for (i in d) {
+          k.push(i);
+          v.push(d[i]);
+      };
+      return (new Function(k, fn.$)).apply(d, v);
     }
-    return data ? fn(data) : fn;
+  };
+  if(!fn.$){
+    fn.$ = 'var $="";';
+    var tpls = tpl.replace(/[\r\t\n]/g, " ").replace(/\'/g,"\\'").split('[:')
+      , i = 0
+    while(i<tpls.length){
+      var p = tpls[i];
+      if(i){
+        var x = p.indexOf(':]');
+        fn.$ += p.substr(0, x);
+        p = p.substr(x+2)
+      }
+      fn.$ += "$+='"+p.replace(/\[\=\:(.*?)\:\]/g, "'+$1+'")+"';";
+      i++;
+    }
+    fn.$ += "return $";
+  }
+
+  return data ? fn(data) : fn;
 }
